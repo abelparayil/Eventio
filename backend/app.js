e5aa1de3cf7ab0a79d0dd711649d05925f693e23
Abel Parayil Abraham <abelabr321@gmail.com>
2023-11-07 16:56:06 +0300

    Login and sign up added for user

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,15 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import userRouter from "./routes/user-routes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/user", userRouter);
 
 mongoose
   .connect(
