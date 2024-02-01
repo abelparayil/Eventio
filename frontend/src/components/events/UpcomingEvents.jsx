580824a01f9b51a430ffc31a209d25004f380b05
Yasin N K <yasinnk15@gmail.com>
2024-02-01 15:37:56 +0530

    initial commit

--- frontend/src/components/events/UpcomingEvents.jsx
+++ frontend/src/components/events/UpcomingEvents.jsx
@@ -2,13 +2,13 @@ import EventCard from "./EventCard";
 
 const UpcomingEvents = () => {
   return (
-    <div className="h-screen">
+    <div className="h-full">
       <div className="p-6 px-8 sp">
         <h1 className=" text-3xl ">
           Upcoming <span className=" text-bluePurple">Events</span>
         </h1>
       </div>
-      <div className="flex p-10 gap-y-6 flex-wrap justify-between ">
+      <div className="grid grid-cols-3 gap-x-8 gap-y-12 flex p-24 gap-y-6 flex-wrap justify-between ">
         <EventCard />
         <EventCard />
         <EventCard />
