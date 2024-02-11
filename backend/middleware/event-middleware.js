6acf39af6c12a5b902fe1568d08cd53d0222ea79
Abel Abraham <abelabr321@gmail.com>
2024-02-11 19:12:26 +0300

    jwt bug fixed with admin middleware

--- backend/middleware/event-middleware.js
+++ backend/middleware/event-middleware.js
@@ -1,3 +1,5 @@
+import jwt from "jsonwebtoken";
+
 export const verifyAdminToken = async (req, res, next) => {
   const token = req.headers.authorization;
 
@@ -5,8 +7,11 @@ export const verifyAdminToken = async (req, res, next) => {
     return res.status(401).json({ message: "Unauthorized: No token provided" });
   }
 
+  const modifiedToken = token.split(" ")[1];
+
   try {
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const decodedToken = jwt.verify(modifiedToken, process.env.JWT_SECRET_KEY);
+    console.log(decodedToken);
     if (decodedToken.role !== "admin") {
       return res.status(403).json({ message: "Forbidden: Admin access only" });
     }
