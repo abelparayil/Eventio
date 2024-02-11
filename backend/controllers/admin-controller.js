12bf66d6228b61a9dc27e70934744fa23e2b11e3
Abel Abraham <abelabr321@gmail.com>
2024-02-11 18:54:08 +0300

    sign error fixed

--- backend/controllers/admin-controller.js
+++ backend/controllers/admin-controller.js
@@ -1,5 +1,6 @@
 import Admin from "../models/Admin.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const adminLogin = async (req, res) => {
   const { email, password } = req.body;
@@ -8,19 +9,23 @@ export const adminLogin = async (req, res) => {
     if (!existingAdmin) {
       return res.status(401).json({ message: "Invalid credentials" });
     }
-    const isValidPassword = await bcrypt.compare(
+
+    const isValidPassword = bcrypt.compareSync(
       password,
       existingAdmin.password
     );
+
     if (!isValidPassword) {
       return res.status(401).json({ message: "Invalid credentials" });
     }
+
     const token = jwt.sign(
       { email: existingAdmin.email, id: existingAdmin._id },
-      process.env.JWT_SECRET,
+      process.env.JWT_SECRET_KEY,
       { expiresIn: "1h" }
     );
-    res.status(200).json({ result: existingAdmin, token });
+
+    res.status(200).json({ message: "Login successful", token });
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
   }
@@ -30,11 +35,16 @@ export const adminSignup = async (req, res) => {
   const { email, password } = req.body;
   try {
     const existingAdmin = await Admin.findOne({ email });
+
     if (existingAdmin) {
       return res.status(400).json({ message: "Admin already exists" });
     }
+
     const hashedPassword = await bcrypt.hash(password, 12);
     const newAdmin = new Admin({ email, password: hashedPassword });
+
+    await newAdmin.save();
+
     res.status(201).json({ message: "Admin created successfully" });
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
