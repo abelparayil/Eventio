6c172d82683aaed4dfac2db8e8ae6f3bb03911fe
Vinay Dev S <vinaydevs@gmail.com>
2024-04-16 10:58:20 +0530

    addes razorpay

--- frontend/src/pages/users/EventDetails.jsx
+++ frontend/src/pages/users/EventDetails.jsx
@@ -5,6 +5,8 @@ import NoImage from "../../assets/images/noimage.jpeg";
 import { isoToNormalDate } from "../../util/TimeConversion";
 import useCommonActions from "../../services/actions/CommonActions";
 import { toast } from "react-toastify";
+import axios from "axios";
+import { loadScript } from "../../util/razorpay";
 
 const URL = "http://localhost:9000";
 
@@ -15,6 +17,62 @@ const EventDetails = () => {
   const user = "VINAY DEV";
   const { id } = useParams();
   const userActions = useCommonActions();
+  async function displayRazorPay(id) {
+    const res = await loadScript(
+      "https://checkout.razorpay.com/v1/checkout.js"
+    );
+
+    if (!res) {
+      alert("Razorpay SDK failed to load. Are you online?");
+      return;
+    }
+
+    const result = await axios.post("/payment/orders", { id: id });
+
+    if (!result) {
+      alert("Server error. Are you online?");
+      return;
+    }
+
+    const { amount, id: order_id, currency } = result.data;
+
+    const options = {
+      key: "<YOUR RAZORPAY KEY>", // Enter the Key ID generated from the Dashboard
+      amount: amount.toString(),
+      currency: currency,
+      name: "Eventio Corp.",
+      description: "Test Transaction",
+
+      order_id: order_id,
+      handler: async function (response) {
+        const data = {
+          orderCreationId: order_id,
+          razorpayPaymentId: response.razorpay_payment_id,
+          razorpayOrderId: response.razorpay_order_id,
+          razorpaySignature: response.razorpay_signature,
+        };
+
+        const result = await axios.post("/payment/success", data);
+
+        alert(result.data.msg);
+      },
+      prefill: {
+        name: "Eventio",
+        email: "eventio@management.com",
+        contact: "9999999999",
+      },
+      notes: {
+        address: "Eventio Corporate Office",
+      },
+      theme: {
+        color: "#61dafb",
+      },
+    };
+
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
+  }
+
   useEffect(() => {
     userActions
       .getEventDetails(id)
@@ -89,19 +147,21 @@ const EventDetails = () => {
           <div className="px-14 py-0 text-xl">
             <p className="text-center p-10">
               Experience the electrifying fusion of technology, culture, and
-              sports at Advitya, VIT Bhopal's flagship annual fest. Get ready
-              for a rollercoaster of innovation and entertainment as Advitya
-              unveils a spectrum of events that ignite creativity, showcase
-              talent, and celebrate the spirit of VIT. Join us in this vibrant
-              extravaganza where every moment is a crescendo of excitement,
-              making Advitya the heartbeat of our campus culture. Let the
-              festivities begin as we redefine the essence of an unforgettable
-              college experience – this is not just an event, it's an Advitya!
+              sports at Advitya, VIT Bhopal&apos;s flagship annual fest. Get
+              ready for a rollercoaster of innovation and entertainment as
+              Advitya unveils a spectrum of events that ignite creativity,
+              showcase talent, and celebrate the spirit of VIT. Join us in this
+              vibrant extravaganza where every moment is a crescendo of
+              excitement, making Advitya the heartbeat of our campus culture.
+              Let the festivities begin as we redefine the essence of an
+              unforgettable college experience – this is not just an event, it's
+              an Advitya!
             </p>
           </div>
           <div className="flex justify-center py-7 ">
             <Button
               name={"Register Now"}
+              onClick={() => displayRazorPay(id)}
               styleclass={"bg-bluePurple rounded text-white px-24"}
             />
           </div>
