31d49788fa31e78bd35393e29c21b0136f708c40
Abel Parayil Abraham <abelabr321@gmail.com>
2024-04-17 18:06:42 +0300

    Get all bookings for QR code

--- backend/routes/booking-routes.js
+++ backend/routes/booking-routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   addBooking,
   deleteBooking,
+  getAllBooking,
   getBookingId,
 } from "../controllers/booking-controller.js";
 import { verifyUserToken } from "../middleware/user-middleware.js";
@@ -11,5 +12,6 @@ const bookingRouter = express.Router();
 bookingRouter.post("/bookEvent/:eventId", verifyUserToken, addBooking);
 bookingRouter.delete("/cancelBooking/:id", verifyUserToken, deleteBooking);
 bookingRouter.get("/getBooking/:eventId", verifyUserToken, getBookingId);
+bookingRouter.get("/getAllBooking", verifyUserToken, getAllBooking);
 
 export default bookingRouter;
