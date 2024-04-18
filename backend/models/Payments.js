d6c40a39aa4df5586a58d693b5c5028e4711b7ef
Abel Parayil Abraham <abelabr321@gmail.com>
2024-04-18 18:48:07 +0300

    Booking error fixed

--- backend/models/Payments.js
+++ backend/models/Payments.js
@@ -14,6 +14,10 @@ const paymentSchema = new Schema({
     type: Boolean,
     default: false,
   },
+  cancelled: {
+    type: Boolean,
+    default: false,
+  },
   userPaid: {
     type: Schema.Types.ObjectId,
     ref: "User",
